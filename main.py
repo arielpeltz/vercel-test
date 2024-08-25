14f394f5f5ac53aa727c167f60191b41ff170b45
Ariel Peltz <ariel.peltz@redis.com>
2024-08-25 14:12:53 +0300

    2 redis

--- main.py
+++ main.py
@@ -9,26 +9,30 @@ load_dotenv('.env.local')
 # Initialize Flask application
 app = Flask(__name__)
 
-# Get Redis URL from environment variable
-redis_url = os.getenv('REDIS_connection_string')
+redis1 = redis.Redis.from_url(os.getenv('REDIS_connection_string'))
+redis2 = redis.Redis.from_url(os.getenv('redis2_connection_string'))
 
-# Connect to Redis using the connection string
-redis_client = redis.Redis.from_url(redis_url)
+
+def val(client, key):
+    value = client.get(key)
+
+    # If key does not exist, set a default value
+    if value is None:
+        value = f"Key '{key}' does not exist in Redis."
+    else:
+        value = value.decode('utf-8')  # Decode the byte value
+
+    return value
 
 
 @app.route('/')
 def index():
-    # Try to get the value of "mykey" from Redis
-    value = redis_client.get('mykey')
-
-    # If key does not exist, set a default value
-    if value is None:
-        value = "Key 'mykey' does not exist in Redis."
-    else:
-        value = value.decode('utf-8')  # Decode the byte value
+    print(val(redis1, 'mykey'))
 
     # Render the value in the template
-    return render_template_string('<h1>{{ value }}</h1>', value=value)
+    return render_template_string('<h1>{{ value1 }} <br> {{ value2 }}</h1>',
+                                  value1=val(redis1, 'mykey'),
+                                  value2=val(redis2, 'mykey'))
 
 
 if __name__ == "__main__":
