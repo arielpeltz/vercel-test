8d6eb1bd295cb3561bb1e5e03293628ee116d63f
Ariel Peltz <ariel.peltz@redis.com>
2024-08-26 19:40:39 +0300

    Remove all the redis stuff

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
 from flask import Flask, render_template_string
-import redis
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env.local
-load_dotenv('.env.local')
 
 # Initialize Flask application
 app = Flask(__name__)
-
-redis1 = redis.Redis.from_url(os.getenv('connection_string'))
-
-
-def val(client, key):
-    value = client.get(key)
-
-    # If key does not exist, set a default value
-    if value is None:
-        value = f"Key '{key}' does not exist in Redis."
-    else:
-        value = value.decode('utf-8')  # Decode the byte value
-
-    return value
 
 
 @app.route('/')
 def index():
     # Render the value in the template
     return render_template_string('<h1>{{ value1 }}</h1>',
-                                  value1=val(redis1, 'headline'))
+                                  value1="Hello World")
 
 
 if __name__ == "__main__":
