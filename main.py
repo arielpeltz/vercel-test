9ca47fb4f0651e7cd8039b800569d279108919c8
Ariel Peltz <ariel.peltz@redis.com>
2024-08-26 19:50:44 +0300

    adding redis

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template_string
+from dotenv import load_dotenv
+import redis
+import os
+
+load_dotenv('.env.local')
+
+redis = redis.Redis.from_url(os.getenv("redis_connection_string"))
+
 
 # Initialize Flask application
 app = Flask(__name__)
@@ -6,9 +14,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    val = redis.get("headline")
+
     # Render the value in the template
     return render_template_string('<h1>{{ value1 }}</h1>',
-                                  value1="Hello World")
+                                  value1=val.decode())
 
 
 if __name__ == "__main__":
